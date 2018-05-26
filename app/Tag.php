9c633d1fc15acee7f9cf9bcde5975aae63a0327c
snarksliveshere <snarksliveshere@gmail.com>
2018-05-26 16:20:56 +0300

    создал модель Tag, дополнил User

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function posts()
+    {
+        $this->belongsToMany(
+            Post::class,
+            'post_tags',
+            'tag_id',
+            'post_id'
+        );
+    }
 }
+
