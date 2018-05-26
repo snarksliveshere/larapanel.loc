1eb979eb7436d0360271682fe89bb42fd2ac4f38
snarksliveshere <snarksliveshere@gmail.com>
2018-05-26 22:11:12 +0300

    очень наскоро натянул шаблон в видах

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        dd('1');
+        return view('admin.dashboard');
     }
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -39,7 +39,7 @@ mix.scripts([
 
 mix.copy('resources/assets/admin/bootstrap/fonts','public/fonts');
 mix.copy('resources/assets/admin/dist/fonts','public/fonts');
-
+mix.copy('resources/assets/admin/dist/img','public/img');
 
 
 
