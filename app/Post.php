e4f4a04efe5df374291b0edfac7494abbbbdc915
snarksliveshere <snarksliveshere@gmail.com>
2018-05-26 16:16:24 +0300

    создал модель Post со связями один к одному и многие ко многим с примерами

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+//        теперь я смогу делать такую штуку
+//        $post = Post::find(1);
+//        $post->category()->title - связь с категорией по hasOne
+
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags', // таблица
+            'post_id', // id этой (пост) модели
+            'tag_id' // id таблицы, с которой связываюсь - тэги $post->tags - все тэги
+        );
+    }
 }
